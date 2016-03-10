0188cb7312875bd0b63aeb39b94e78cedab9e5c6
unknown <100525235@UOSL15-R90266WC.oncampus.local>
2016-03-10 00:45:32 -0500

    GUI now reads in from text file WordProbabilities

--- DataSource.java
+++ DataSource.java
@@ -1,23 +1,36 @@
 import javafx.collections.*;
+import java.util.*;
+import java.io.*;
 
-public class DataSource {
-    public static ObservableList<Student> getAllStudents() {
-        ObservableList<Student> students = FXCollections.observableArrayList();
-        
-        students.add(new Student("abcdefghijk", "Ham", 2.85));
-        students.add(new Student("abcdefghijk", "Ham", 1.71));
-        students.add(new Student("abcdefghijk", "Ham", 3.60));
-        students.add(new Student("abcdefghijk", "Ham", 2.19));
-        students.add(new Student("abcdefghijk", "Ham", 3.15));
-        students.add(new Student("abcdefghijk", "Ham", 2.45));
-        students.add(new Student("abcdefghijk", "Ham", 3.15));
-        students.add(new Student("abcdefghijk", "Ham", 1.55));
-        students.add(new Student("abcdefghijk", "Ham", 1.35));
-        students.add(new Student("abcdefghijk", "Ham", 2.64));
-        students.add(new Student("abcdefghijk", "Ham", 3.13));
-        students.add(new Student("abcdefghijk", "Ham", 2.40));
-        students.add(new Student("abcdefghijk", "Ham", 1.95));
+public class DataSource
+{
+	public static ObservableList<Student> getAllStudents()
+	{
+		ObservableList<Student> prob= FXCollections.observableArrayList();
+		try
+		{
+			Scanner sc = new Scanner(new File("WordProbabilities"));
+			while (sc.hasNext())
+			{
+				String fName = sc.next();
+				Double val = Double.parseDouble(sc.next());
+				String type = "";
+				if (val > 0.75 )
+				{
+					type = "Spam";
+				}
+				else 
+				{
+					type = "ham";
+				}
+				prob.add(new Student(fName,type,val));
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 
-        return students;
-    }
+		return prob;
+	}
 }
